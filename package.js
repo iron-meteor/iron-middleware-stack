80c1dc1b34dbd9a7f92ec18062ba817bd8051afa
Chris Mather <mather.chris@gmail.com>
2014-09-15 22:33:06 -0700

    bump version to 1.0.0-pre1

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   summary: "Client and server middleware support inspired by Connect.",
-  version: "1.0.0-pre0",
+  version: "1.0.0-pre1",
   git: "https://github.com/eventedmind/iron-middleware-stack"
 });
 
 Package.on_use(function (api) {
-  api.use('iron:core@1.0.0-pre0');
+  api.use('iron:core@1.0.0-pre1');
   api.imply('iron:core');
 
-  api.use('iron:url@1.0.0-pre0');
+  api.use('iron:url@1.0.0-pre1');
 
   api.add_files('lib/handler.js');
   api.add_files('lib/middleware_stack.js');
